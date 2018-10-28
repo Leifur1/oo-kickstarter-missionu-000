4837d047f8806923b62ffa91d584ee66bd800883
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-28 00:17:12 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,8 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project
-    @backed_projects << Project.new(title)
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
   end
 end
